3e5b1f5e23c087199eae4c6f54ad5b3056c661a3
cecdarwiche <129707322+cecdarwiche@users.noreply.github.com>
2025-03-23 01:17:35 -0400

    Midterm

--- Miami/miami.js
+++ Miami/miami.js
@@ -3,6 +3,8 @@ const express = require('express')
 
 // app variable so we don't have to type express all the time
 const app = express()
+//select static routing
+app.use(express.static('./public'))
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended:true}))
@@ -22,12 +24,66 @@ app.set('view engine', 'handlebars');
 const port = process.env.port || 3000
 
 let navigation = require("./data/navigation.json")
+
+//import slideshow data
+let slideshow = require("./data/slideshow.json")
+
+//import gallery data
+let gallery = require('./data/gallery.json')
+
+//import page data
+let content = require('./data/pages.json')
+
+//import destinations
+let destinations = require('./data/destinations.json')
+
 // create some routes
 //  SYNTAX: app.get('pass parameter we want to request', (request, response) =>...)
 app.get('/', (request, response) =>{
+//filter slideshow object to get home page only
+    let slides = slideshow.slides.filter((slide) => {
+        return slide.home == true; 
+    })
+
     response.type("text/html")
     // we don't put status here because default status is already 200 meaning it is all good, only overwrite when we have a problem
-    response.render("home",{title:"Miami Travel Site", nav: navigation})
+    response.render("page",{
+        title:"Welcome to Miami", 
+        nav: navigation,
+        slides: slides,
+        images: gallery.images 
+    })
+})
+
+//dynamic routes for pages
+app.get('/page/:page', (req,res) => {
+    //filter pages object to get page from :page req.params.page
+    let page = content.pages.filter((item) => {
+        return item.page == req.params.page 
+    })
+    //page is an array with just one item, we access the position 0 to get the object alone
+    console.log(page[0]);
+
+    //filter slideshow object to get home page only
+    let slides = slideshow.slides.filter((slide) => {
+        return slide.home == true; 
+    })
+
+    //filter destinations
+    let dest = destinations.locations.filter((loc) => {
+        return loc.page == req.params.page; 
+    })
+
+    res.type("text/html")
+    res.render("page",{
+        title: page[0].title, 
+        description: page[0].description,
+        locations: dest, 
+        nav: navigation,
+        slides: slides,
+        images: gallery.images
+    })
+
 })
 
 app.get('/beaches', (request, response) =>{
@@ -39,6 +95,17 @@ app.get('/nightlife', (request, response) =>{
     response.type("text/html")
     response.render("page",{title: "Miami Nightlife", nav: navigation})
 })
+
+app.get('/food', (request, response) =>{
+    response.type("text/html")
+    response.render("page",{title: "Miami Food", nav: navigation})
+})
+
+app.get('/shopping', (request, response) =>{
+    response.type("text/html")
+    response.render("page",{title: "Miami Shopping", nav: navigation})
+})
+
 
 //generating an ERROR, there is no respon
 app.get('/about', (request, response) =>{
@@ -93,3 +160,17 @@ app.listen(port, () =>{
     console.log(` press Ctrl-C to terminate. `)
 
 })
+
+
+/*
+to add newsletter, add this to the navigation.json file:
+
+{
+            "text": "Newsletter Sign Up",
+            "href":"/newsletter-signup"
+        },{
+            "text": "Newsletter List",
+            "href":"/newsletter/list"
+        },
+*/
+        
